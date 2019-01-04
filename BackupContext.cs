fcf25f72ce0f0faad58ce9bb6ecff2b91525fcee
Marble Wu <huobazi@gmail.com>
2019-01-04 11:02:21 +0800

    reflactor for singleton context create.

--- BackupContext.cs
+++ BackupContext.cs
@@ -1,21 +1,44 @@
 
+using System;
 public sealed class BackupContext
 {
-    public static readonly BackupContext instance;
-
-    static BackupContext()
+    private static object _mutex = new object();
+    private static BackupContext instance;
+    private BackupContext() { }
+    private BackupContext(string logFile, string tmpPath)
     {
-        instance = new BackupContext();
+        this.LogFile = logFile;
+        this.TmpPath = tmpPath;
     }
-
-    public static BackupContext Current {
+    public static BackupContext Create(string logFile, string tmpPath)
+    {
+        if (instance != null)
+        {
+            throw new InvalidOperationException("BackupContext already created - use BacupContext.Current to get");
+        }
+        else
+        {
+            lock (_mutex)
+            {
+                if (instance == null)
+                {
+                    instance = new BackupContext(logFile, tmpPath);
+                }
+            }
+        }
+        return instance;
+    }
+    public static BackupContext Current
+    {
         get
         {
+            if (instance == null)
+            {
+                throw new InvalidOperationException("BackupContext not created - use BacupContext.Create to create");
+            }
             return instance;
         }
     }
-
-    public string LogFile{get;set;}
-
-    public string TmpPath{get;set;}
+    public string LogFile { get; private set; }
+    public string TmpPath { get; private set; }
 }
