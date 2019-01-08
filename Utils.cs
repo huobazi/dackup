c1b8a76957a8c3bce24760f715541b3f1ff5fb6c
Marble Wu <huobazi@gmail.com>
2019-01-08 22:08:34 +0800

    add source src.

--- Utils.cs
+++ Utils.cs
@@ -62,18 +62,18 @@ namespace dackup
             }
         }
 
+        // https://github.com/icsharpcode/SharpZipLib/wiki/GZip-and-Tar-Samples#createTGZ
         public static void CreateTarGZ(string tgzFilename, string sourceDirectory)
         {
             Stream outStream = File.Create(tgzFilename);
             Stream gzoStream = new GZipOutputStream(outStream);
             TarArchive tarArchive = TarArchive.CreateOutputTarArchive(gzoStream);
 
-            // Note that the RootPath is currently case sensitive and must be forward slashes e.g. "c:/temp"
-            // and must not end with a slash, otherwise cuts off first char of filename
-            // This is scheduled for fix in next release
             tarArchive.RootPath = sourceDirectory.Replace('\\', '/');
             if (tarArchive.RootPath.EndsWith("/"))
+             {
                 tarArchive.RootPath = tarArchive.RootPath.Remove(tarArchive.RootPath.Length - 1);
+             }
 
             AddDirectoryFilesToTar(tarArchive, sourceDirectory, true);
 
@@ -82,12 +82,9 @@ namespace dackup
 
         private static void AddDirectoryFilesToTar(TarArchive tarArchive, string sourceDirectory, bool recurse)
         {
-            // Optionally, write an entry for the directory itself.
-            // Specify false for recursion here if we will add the directory's files individually.
             TarEntry tarEntry = TarEntry.CreateEntryFromFile(sourceDirectory);
             tarArchive.WriteEntry(tarEntry, false);
 
-            // Write each file to the tar.
             string[] filenames = Directory.GetFiles(sourceDirectory);
             foreach (string filename in filenames)
             {
