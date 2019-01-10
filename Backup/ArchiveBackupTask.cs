2d2b682da75a7e36dc9b55417d0bdf401460960f
Marble Wu <huobazi@gmail.com>
2019-01-10 19:46:32 +0800

    fix .gitignore.

---
+++ Backup/ArchiveBackupTask.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+
+using Serilog;
+
+namespace dackup
+{
+    public class ArchiveBackupTask : BackupTaskBase
+    {
+        private string name;
+        private List<string> includePathList;
+        private List<string> excludePathList;
+
+        private ArchiveBackupTask() { }
+
+        public ArchiveBackupTask(string name, List<string> includePathList, List<string> excludePathList)
+        {
+            this.name = name;
+            this.includePathList = includePathList;
+            this.excludePathList = excludePathList;
+        }
+
+        protected override BackupTaskResult Backup()
+        {
+            if (includePathList == null || includePathList.Count <= 0)
+            {
+                return new BackupTaskResult
+                {
+                    Result = false,
+                    Message = "No archives files setting"
+                };
+            }
+            Log.Information($"Archive backup start");
+
+            var directory = Path.Combine(DackupContext.Current.TmpPath, "archives");
+            Directory.CreateDirectory(directory);
+
+            this.includePathList.ForEach(file =>
+            {
+                Log.Information($"Archive backup : {file}");
+
+                FileAttributes attr = File.GetAttributes(file);
+
+                if (attr.HasFlag(FileAttributes.Directory))
+                {
+                    var destDirectory = Path.Combine(directory, file.TrimStart('/'));
+                    Utils.DirectoryCopy(file, destDirectory);
+                }
+                else
+                {
+                    var destFile = Path.Combine(directory, file.TrimStart('/'));
+                    var destDirectory = destFile.Substring(0, destFile.LastIndexOf('/') + 1);
+                    Directory.CreateDirectory(destDirectory);
+
+                    File.Copy(file, destFile, true);
+                }
+            });
+
+            var tgzFileName  = Path.Combine(Path.Combine(DackupContext.Current.TmpPath, $"archives_{this.name}_{DateTime.Now:s}.tar.gz"));
+
+            Utils.CreateTarGZ(tgzFileName,directory);
+
+            return new BackupTaskResult
+            {
+                Result = true,
+                FilesList = new List<string> { tgzFileName },
+            };
+        }
+    }
+}
---
+++ Backup/IBackupTask.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+
+namespace dackup
+{
+    public interface IBackupTask
+    {
+        Task<BackupTaskResult> BackupAsync();
+    }
+
+    public class BackupTaskResult
+    {
+        public bool Result { get; set; }
+        public List<string> FilesList { get; set; }
+        public string Message { get; set; }
+    }
+}
