923a7a0123da9e19e605ac6b5aa2a9577bac45c1
Marble Wu <huobazi@gmail.com>
2020-06-29 20:58:07 +0800

    IEnumerable<T>.ForEach should not throw exception when source or action is null

--- src/Extensions/IEnumerableExtensions.cs
+++ src/Extensions/IEnumerableExtensions.cs
@@ -7,9 +7,11 @@ namespace dackup.Extensions
     {
         public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
-            if (source == null) throw new ArgumentNullException(nameof(source));
-            if (action == null) throw new ArgumentNullException(nameof(action));
-
+            if (source == null || action == null ) 
+            {
+                return;
+            }
+            
             foreach (var element in source)
             {
                 action(element);
@@ -18,8 +20,10 @@ namespace dackup.Extensions
 
         public static void ForEach<T>(this IEnumerable<T> source, Action<T, int> action)
         {
-            if (source == null) throw new ArgumentNullException(nameof(source));
-            if (action == null) throw new ArgumentNullException(nameof(action));
+            if (source == null || action == null ) 
+            {
+                return;
+            }
 
             var index = 0;
             foreach (var element in source)
